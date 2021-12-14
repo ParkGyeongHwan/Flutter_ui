5ae7d00ada89f93c7c159f2fe16eb186b106925f
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-12-14 12:35:45 +0900

    껍데기만들어보기

---
+++ image_search/lib/data/fake_data.dart
@@ -0,0 +1,11 @@
+import 'package:image_search/model/photo.dart';
+
+final fakePhotos =[
+  Photo(
+previewURL: "https://cdn.pixabay.com/photo/2015/08/13/20/06/flower-887443_150.jpg",),
+
+Photo(
+previewURL: "https://pixabay.com/get/g7974c9255c2a996c6f82806b8666100cfe0ecbe46e008d4a617f99ee5259397ad7fddb4ec3f0b4b18a807745cf0c3bb2_640.jpg",),
+Photo(
+previewURL: "https://cdn.pixabay.com/user/2017/07/18/13-46-18-393_250x250.jpg"),
+];
