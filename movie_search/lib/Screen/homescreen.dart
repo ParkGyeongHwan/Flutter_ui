1769610cf171d238a8d99ff7320ba74c56e8bc69
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-12-22 10:59:08 +0900

    ListView.builder 수정

--- movie_search/lib/Screen/homescreen.dart
+++ movie_search/lib/Screen/homescreen.dart
@@ -1,10 +1,63 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Home_Screen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:movie_search/data/movie_api.dart';
+
+class Home_Screen extends StatefulWidget {
   const Home_Screen({Key? key}) : super(key: key);
 
   @override
+  State<Home_Screen> createState() => _Home_ScreenState();
+}
+
+class _Home_ScreenState extends State<Home_Screen> {
+  List<Results> Movies = [];
+  Future<Movie> fetchMovie() async {
+    final response = await http.get(Uri.parse(
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
+
+    if (response.statusCode == 200) {
+      return Movie.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+  late Future<Movie> futureMovie;
+  @override
+  void initState() {
+    super.initState();
+    futureMovie = fetchMovie();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('영화 찾기'),
+        ),
+        body: FutureBuilder<Movie>(
+          future: futureMovie,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.results.length,
+                itemBuilder: (context, index) {
+                  String poster = snapshot.data!.results[index].posterPath;
+                  return Column(
+                    children: [
+                      Image.network('https://image.tmdb.org/t/p/w500$poster'),
+                      Text(snapshot.data!.results[index].title),
+                    ],
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const CircularProgressIndicator();
+          },
+        ));
   }
 }
