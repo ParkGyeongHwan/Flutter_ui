92f48ba52116aeea89da738d6255231cb192dcee
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-12-22 14:02:04 +0900

    api 요소수 실습

--- movie_search/lib/Screen/homescreen.dart
+++ movie_search/lib/Screen/homescreen.dart
@@ -12,7 +12,9 @@ class Home_Screen extends StatefulWidget {
 }
 
 class _Home_ScreenState extends State<Home_Screen> {
+  final TextEditingController _textEditingController = TextEditingController();
   List<Results> Movies = [];
+
   Future<Movie> fetchMovie() async {
     final response = await http.get(Uri.parse(
         'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
@@ -25,6 +27,7 @@ class _Home_ScreenState extends State<Home_Screen> {
   }
 
   late Future<Movie> futureMovie;
+
   @override
   void initState() {
     super.initState();
@@ -37,31 +40,51 @@ class _Home_ScreenState extends State<Home_Screen> {
         appBar: AppBar(
           title: Text('영화 찾기'),
         ),
-        body: FutureBuilder<Movie>(
-          future: futureMovie,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-                  childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                icon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    String search = _textEditingController.text;
+                  },
                 ),
-                itemCount: snapshot.data!.results.length,
-                itemBuilder: (context, index) {
-                  String poster = snapshot.data!.results[index].posterPath;
-                  return Column(
-                    children: [
-                      Image.network('https://image.tmdb.org/t/p/w500$poster'),
-                      Text(snapshot.data!.results[index].title),
-                    ],
-                  );
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.teal)),
+                hintText: '영화를 검색하세요',
+              ),
+              controller: _textEditingController,
+            ),
+            Expanded(
+              child: FutureBuilder<Movie>(
+                future: futureMovie,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+                        childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
+                      ),
+                      itemCount: snapshot.data!.results.length,
+                      itemBuilder: (context, index) {
+                        String poster =
+                            snapshot.data!.results[index].posterPath;
+                        return Column(
+                          children: [
+                            Image.network(
+                                'https://image.tmdb.org/t/p/w500$poster'),
+                            Text(snapshot.data!.results[index].title),
+                          ],
+                        );
+                      },
+                    );
+
                 },
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          },
+              ),
+            ),
+          ],
         ));
   }
 }
